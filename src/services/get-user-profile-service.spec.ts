b6528d792119ff96f78c63bb5eff1f9163a78ea6
CharlesPereira <charlesti.ptu@gmail.com>
2023-04-05 20:19:55 -0300

    test: profile findByid

---
+++ src/services/get-user-profile-service.spec.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+import { expect, describe, it, beforeEach } from 'vitest'
+import { hash } from 'bcryptjs'
+
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { GetUserProfileService } from './get-user-profile-service'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileService
+
+// sut -> syster under test = default tester
+
+describe('Get User Profile Service', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileService(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.name).toEqual('John Doe')
+  })
+
+  it('should be able to get user profile with wrong id', async () => {
+    expect(
+      async () =>
+        await sut.execute({
+          userId: 'non-existing-id',
+        })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
