3880a74d4f9c9f8252afda90a5a42e574883da5c
CharlesPereira <charlesti.ptu@gmail.com>
2023-04-18 00:15:01 -0300

    test: fetch user check ins history paginate

--- src/services/fetch-user-check-ins-history-service.spec.ts
+++ src/services/fetch-user-check-ins-history-service.spec.ts
@@ -18,15 +18,41 @@ describe('Fetch Check-in History Service', () => {
       gym_id: 'gym-01',
       user_id: 'user-01',
     })
+
     await checkInsRepository.create({
-      gym_id: 'gym-01',
+      gym_id: 'gym-02',
       user_id: 'user-01',
     })
 
     const { checkIns } = await sut.execute({
-      gymId: 'gym-01',
+      userId: 'gym-01',
+      page: 1,
     })
 
-    expect(checkIn.id).toEqual(expect.any(String))
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-01' }),
+      expect.objectContaining({ gym_id: 'gym-02' }),
+    ])
+  })
+
+  it('should be able to fetch paginated user check-in history', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await checkInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: 'user-01',
+      })
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: 'gym-01',
+      page: 2,
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-21' }),
+      expect.objectContaining({ gym_id: 'gym-22' }),
+    ])
   })
 })
