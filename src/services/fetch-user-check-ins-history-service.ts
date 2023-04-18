dbeed97d5b9f354a9d8d2f45742d551789ac7770
CharlesPereira <charlesti.ptu@gmail.com>
2023-04-18 00:14:09 -0300

    chore: fetch user check ins history paginate

--- src/services/fetch-user-check-ins-history-service.ts
+++ src/services/fetch-user-check-ins-history-service.ts
@@ -4,6 +4,7 @@ import { CheckInsRepository } from '@/repositories/check-ins-repository'
 
 interface FetchUserCheckInsHistoryServiceRequest {
   userId: string
+  page: number
 }
 
 interface FetchUserCheckInsHistoryServiceResponse {
@@ -15,8 +16,9 @@ export class FetchUserCheckInsHistoryServices {
 
   async execute({
     userId,
+    page,
   }: FetchUserCheckInsHistoryServiceRequest): Promise<FetchUserCheckInsHistoryServiceResponse> {
-    const checkIns = await this.checkInRepository.findManyByUserId(userId)
+    const checkIns = await this.checkInRepository.findManyByUserId(userId, page)
 
     return { checkIns }
   }
