815930228df1190348d42d18211205806152a1de
CharlesPereira <charlesti.ptu@gmail.com>
2023-04-25 23:11:39 -0300

    feat: validate the check-in

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,8 +3,10 @@ import { CheckIn, Prisma } from '@prisma/client'
 // CheckInUncheckedCreateInput -> utilizando pois o prisma entende que o user_id já foi criado
 
 export interface CheckInsRepository {
-  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findById(id: string): Promise<CheckIn | null>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   countByUserId(userId: string): number
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  save(checkIn: CheckIn): Promise<CheckIn>
 }
